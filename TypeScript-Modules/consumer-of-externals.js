9b7ed2ddfdcb452b5eb370120ff7b92fb8b80b47
Gordey Doronin <g.doronin@gmail.com>
2014-11-18 17:21:08 +0600

    JavaScript files to be excluded. Only TypeScript to be stored in repo.

--- TypeScript-Modules/consumer-of-externals.js
+++
@@ -1,8 +0,0 @@
-﻿///// <reference path="external-module.d.ts" />
-define(["require", "exports"], function(require, exports) {
-    var some = {
-        field1: 'ex1',
-        field2: 'ex2'
-    };
-});
-//# sourceMappingURL=consumer-of-externals.js.map
